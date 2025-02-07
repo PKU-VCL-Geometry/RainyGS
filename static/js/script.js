593f34ce0282dc937598c4185bd1a342486b590f
Daiqy <qiyudai@pku.edu.cn>
2025-02-08 01:47:23 +0800

    temp offline repair

--- static/js/script.js
+++ static/js/script.js
@@ -1,63 +1,203 @@
-class BeforeAfter {
-  constructor(enteryObject) {
+// This is based on: http://thenewcode.com/364/Interactive-Before-and-After-Video-Comparison-in-HTML5-Canvas
+// With additional modifications based on: https://jsfiddle.net/7sk5k4gp/13/
 
-      const beforeAfterContainer = document.querySelector(enteryObject.id);
-      const before = beforeAfterContainer.querySelector('.bal-before');
-      const beforeText = beforeAfterContainer.querySelector('.bal-beforePosition');
-      const afterText = beforeAfterContainer.querySelector('.bal-afterPosition');
-      const handle = beforeAfterContainer.querySelector('.bal-handle');
-      var widthChange = 0;
+function playVids(videoId) {
+    var videoMerge = document.getElementById(videoId + "Merge");
+    var vid = document.getElementById(videoId);
 
-    // beforeAfterContainer.querySelector('.bal-before-inset').setAttribute("style", "width: " + beforeAfterContainer.offsetWidth + "px;")
-    // window.onresize = function () {
-    //     beforeAfterContainer.querySelector('.bal-before-inset').setAttribute("style", "width: " + beforeAfterContainer.offsetWidth + "px;")
-    // }
-      beforeAfterContainer.querySelector('.bal-before-inset').setAttribute("style", "width: " + (beforeAfterContainer.offsetWidth * 2) + "px;")
-    //   window.onresize = function () {
-    //      beforeAfterContainer.querySelector('.bal-before-inset').setAttribute("style", "width: " + (beforeAfterContainer.offsetWidth * 2) + "px;")
-    //   }
-    window.addEventListener('resize', function () {
-        beforeAfterContainer.querySelector('.bal-before-inset').setAttribute("style", "width: " + (beforeAfterContainer.offsetWidth * 2) + "px;");
-    });
+    var position = 0.5;
+    var vidWidth = vid.videoWidth / 2;
+    var vidHeight = vid.videoHeight;
 
-      before.setAttribute('style', "width: 50%;");
-      handle.setAttribute('style', "left: 50%;");
+    // var widthRatio = vid.videoWidth / 2532;
+    // var heightRatio = vid.videoHeight / 832; 
+    // var vidWidth = vid.videoWidth / (2 * widthRatio);
+    // var vidHeight = vid.videoHeight / (1 * heightRatio);
 
-      //touch screen event listener
-      beforeAfterContainer.addEventListener("touchstart", (e) => {
+    var mergeContext = videoMerge.getContext("2d");
 
-          beforeAfterContainer.addEventListener("touchmove", (e2) => {
-              let containerWidth = beforeAfterContainer.offsetWidth;
-              let currentPoint = e2.changedTouches[0].clientX;
+    
+    if (vid.readyState > 3) {
+        vid.play();
 
-              let startOfDiv = beforeAfterContainer.offsetLeft;
+        function trackLocation(e) {
+            // Normalize to [0, 1]
+            bcr = videoMerge.getBoundingClientRect();
+            position = ((e.pageX - bcr.x) / bcr.width);
+        }
+        function trackLocationTouch(e) {
+            // Normalize to [0, 1]
+            bcr = videoMerge.getBoundingClientRect();
+            position = ((e.touches[0].pageX - bcr.x) / bcr.width);
+        }
 
-              let modifiedCurrentPoint = currentPoint - startOfDiv;
+        videoMerge.addEventListener("mousemove",  trackLocation, false); 
+        videoMerge.addEventListener("touchstart", trackLocationTouch, false);
+        videoMerge.addEventListener("touchmove",  trackLocationTouch, false);
 
-              if (modifiedCurrentPoint > 10 && modifiedCurrentPoint < beforeAfterContainer.offsetWidth - 10) {
-                  let newWidth = modifiedCurrentPoint * 100 / containerWidth;
 
-                  before.setAttribute('style', "width:" + newWidth + "%;");
-                  afterText.setAttribute('style', "z-index: 1;");
-                  handle.setAttribute('style', "left:" + newWidth + "%;");
-              }
-          });
-      });
+        function drawLoop() {
+            mergeContext.drawImage(vid, 0, 0, vidWidth, vidHeight, 0, 0, vidWidth, vidHeight);
+            var colStart = (vidWidth * position).clamp(0.0, vidWidth);
+            var colWidth = (vidWidth - (vidWidth * position)).clamp(0.0, vidWidth);
+            mergeContext.drawImage(vid, colStart + vidWidth, 0, colWidth, vidHeight, colStart, 0, colWidth, vidHeight);
+            requestAnimationFrame(drawLoop);
+            
 
-      //mouse move event listener
-      beforeAfterContainer.addEventListener('mousemove', (e) => {
-          let containerWidth = beforeAfterContainer.offsetWidth;
-          widthChange = e.offsetX;
-          let newWidth = widthChange * 100 / containerWidth;
+            //// Divider Line
+
+            // Hyperparameters
+            var circle_radius = 50;         // Circle radius
+            var stroke_color = "#FFFFFF";   // Line color (white)
+            var line_width = 4;             // Line width
+
+            mergeContext.beginPath();
+            // Get the center position and radius of the circle
+            var circleCenterX = vidWidth * position;
+            var circleRadius = circle_radius;
+            // Draw the divider line: top part
+            mergeContext.moveTo(circleCenterX, 0);
+            mergeContext.lineTo(circleCenterX, vidHeight / 2 - circleRadius);  // To the top of the circle
+            // Draw the divider line: bottom part
+            mergeContext.moveTo(circleCenterX, vidHeight / 2 + circleRadius);  // Start from below the circle
+            mergeContext.lineTo(circleCenterX, vidHeight);  // Continue to the bottom
+            mergeContext.closePath();
+            mergeContext.strokeStyle = stroke_color;
+            mergeContext.lineWidth = line_width;
+            mergeContext.stroke();
+
+            //// Circle
+            mergeContext.beginPath();
+            mergeContext.arc(circleCenterX, vidHeight / 2, circleRadius, 0, Math.PI * 2, false);
+            mergeContext.fillStyle = "rgba(255, 255, 255, 0.2)"; // White with transparency
+            mergeContext.fill();
+            mergeContext.closePath();
+            // Add white border to the circle
+            mergeContext.lineWidth = line_width; // Set border width
+            mergeContext.strokeStyle = stroke_color; // White border
+            mergeContext.stroke(); // Draw the border
+            
+            //// Arrows
+            // Left arrow
+            mergeContext.beginPath();
+            mergeContext.moveTo(vidWidth * position - 12, vidHeight / 2 - 15); // Left arrow start
+            mergeContext.lineTo(vidWidth * position - 25, vidHeight / 2); // Left arrow point
+            mergeContext.lineTo(vidWidth * position - 12, vidHeight / 2 + 15); // Left arrow end
+            mergeContext.closePath();
+            mergeContext.fillStyle = stroke_color; // White arrow
+            mergeContext.fill();
+            // Right arrow
+            mergeContext.beginPath();
+            mergeContext.moveTo(vidWidth * position + 12, vidHeight / 2 - 15); // Right arrow start
+            mergeContext.lineTo(vidWidth * position + 25, vidHeight / 2); // Right arrow point
+            mergeContext.lineTo(vidWidth * position + 12, vidHeight / 2 + 15); // Right arrow end
+            mergeContext.closePath();
+            mergeContext.fillStyle = stroke_color; // White arrow
+            mergeContext.fill();
+            
+            //// Text Labels
+            // Hyperparameters
+            var padding = 21;
+
+            var rectX = 50; // X position of the background
+            var rectY = vidHeight - 120; // Y position of the background
+            var rectWidth_left = 220; // Width of the rectangle (adjustable)
+            var text_left = "Original";
+
+            var rectWidth_right = 220;
+            var rectXRight = vidWidth - rectWidth_right - 50;  // Adjust X for the right label, so it appears on the right side
+            var rectYRight = rectY;  // Same Y position for consistency
+            var text_right = "RainyGS";
+
+            var rectHeight = 80; // Height of the rectangle (adjustable)
+            var cornerRadius = 8; // Radius for rounded corners
+            var rect_color = "#121212"
+            var font_color = "#FFFFFF";
+            var font_style = "3em 'Noto Sans', sans-serif"
+
+
+            // Clip the region to ensure text is hidden based on position
+            mergeContext.save();
+            mergeContext.beginPath();
+            mergeContext.rect(0, 0, vidWidth * position - line_width / 2, vidHeight);
+            mergeContext.clip();  // This will mask the left portion, hiding the text on the left side of the line.
+
+            // Set fill color for the background
+            mergeContext.fillStyle = rect_color; // Black background
+            // Draw the rounded rectangle
+            mergeContext.beginPath();
+            mergeContext.moveTo(rectX + cornerRadius, rectY); // Move to top left corner with radius
+            mergeContext.lineTo(rectX + rectWidth_left - cornerRadius, rectY); // Top edge
+            mergeContext.arcTo(rectX + rectWidth_left, rectY, rectX + rectWidth_left, rectY + rectHeight, cornerRadius); // Top-right corner
+            mergeContext.lineTo(rectX + rectWidth_left, rectY + rectHeight - cornerRadius); // Right edge
+            mergeContext.arcTo(rectX + rectWidth_left, rectY + rectHeight, rectX + rectWidth_left - cornerRadius, rectY + rectHeight, cornerRadius); // Bottom-right corner
+            mergeContext.lineTo(rectX + cornerRadius, rectY + rectHeight); // Bottom edge
+            mergeContext.arcTo(rectX, rectY + rectHeight, rectX, rectY + rectHeight - cornerRadius, cornerRadius); // Bottom-left corner
+            mergeContext.lineTo(rectX, rectY + cornerRadius); // Left edge
+            mergeContext.arcTo(rectX, rectY, rectX + cornerRadius, rectY, cornerRadius); // Top-left corner
+
+            mergeContext.closePath();
+            mergeContext.fill(); // Fill the background
+
+            // Left text label
+            mergeContext.fillStyle = font_color; // Set text color to white
+            mergeContext.font = font_style; // Set font style
+            mergeContext.fillText(text_left, rectX + padding, rectY + rectHeight - padding); // Draw text inside the background
+            mergeContext.restore();
+
+
+            // Right text label (RainyGS)
+            mergeContext.save();
+            mergeContext.beginPath();
+            mergeContext.rect(vidWidth * position + line_width / 2, 0, vidWidth - vidWidth * position, vidHeight);  // Clip the region to mask the right side based on the position
+            mergeContext.clip();  // This will mask the right portion, hiding the text on the right side of the line.
+            // Draw the rounded rectangle for the right label (RainyGS)
+            mergeContext.fillStyle = rect_color; // Black background
+            // Draw the rounded rectangle for the right label
+            mergeContext.beginPath();
+            mergeContext.moveTo(rectXRight + cornerRadius, rectYRight); // Move to top left corner with radius
+            mergeContext.lineTo(rectXRight + rectWidth_right - cornerRadius, rectYRight); // Top edge
+            mergeContext.arcTo(rectXRight + rectWidth_right, rectYRight, rectXRight + rectWidth_right, rectYRight + rectHeight, cornerRadius); // Top-right corner
+            mergeContext.lineTo(rectXRight + rectWidth_right, rectYRight + rectHeight - cornerRadius); // Right edge
+            mergeContext.arcTo(rectXRight + rectWidth_right, rectYRight + rectHeight, rectXRight + rectWidth_right - cornerRadius, rectYRight + rectHeight, cornerRadius); // Bottom-right corner
+            mergeContext.lineTo(rectXRight + cornerRadius, rectYRight + rectHeight); // Bottom edge
+            mergeContext.arcTo(rectXRight, rectYRight + rectHeight, rectXRight, rectYRight + rectHeight - cornerRadius, cornerRadius); // Bottom-left corner
+            mergeContext.lineTo(rectXRight, rectYRight + cornerRadius); // Left edge
+            mergeContext.arcTo(rectXRight, rectYRight, rectXRight + cornerRadius, rectYRight, cornerRadius); // Top-left corner
+
+            mergeContext.closePath();
+            mergeContext.fill(); // Fill the background
+
+            // Draw the right text inside the background
+            mergeContext.fillStyle = font_color; // Set text color to white
+            mergeContext.font = font_style; // Set font style
+            mergeContext.fillText(text_right, rectXRight + padding, rectYRight + rectHeight - padding);
+
+            mergeContext.restore();
+
+        }   
+
+        requestAnimationFrame(drawLoop);
+    } 
+}
+
+Number.prototype.clamp = function(min, max) {
+return Math.min(Math.max(this, min), max);
+};
+    
+function resizeAndPlay(element)
+{
+    var cv = document.getElementById(element.id + "Merge");
+    cv.width = element.videoWidth / 2;
+    cv.height = element.videoHeight;
+
+    // var widthRatio = element.videoWidth / 2532;
+    // var heightRatio = element.videoHeight / 832; 
+    // cv.width = element.videoWidth / (2 * widthRatio);
+    // cv.height = element.videoHeight / (1 * heightRatio);
+
+    element.play();
+    element.style.height = "0px";  // Hide video without stopping it
         
-          //document.getElementById('widthDisplay').textContent = modifiedCurrentPoint;
-
-          if (e.offsetX > 10 && e.offsetX < beforeAfterContainer.offsetWidth - 10) {
-              before.setAttribute('style', "width:" + newWidth + "%;");
-              afterText.setAttribute('style', "z-index:" + "1;");
-              handle.setAttribute('style', "left:" + newWidth + "%;");
-          }
-      })
-
-  }
+    playVids(element.id);
 }
